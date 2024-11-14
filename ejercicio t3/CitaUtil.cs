a30065720a396f339dfbe202573d645aae8bdfed
David-j21 <davidjauregui2110@gmail.com>
2024-11-14 12:04:38 -0500

    a

---
+++ ejercicio t3/CitaUtil.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_t3
+{
+    internal class CitaUtil
+    {
+        public static void CrearCita(Cita[] citas, ref int contador)
+        {
+            Console.Write("Ingrese número de cita: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese código del estudiante: ");
+            int codigo = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese nombre del estudiante: ");
+            string nombre = Console.ReadLine();
+            Console.Write("Ingrese universidad del estudiante: ");
+            string universidad = Console.ReadLine();
+            Estudiante estudiante = new Estudiante(codigo, nombre, universidad);
+
+            Console.Write("Ingrese enfermedad: ");
+            string enfermedad = Console.ReadLine();
+            Console.Write("Ingrese precio de la cita: ");
+            double precio = double.Parse(Console.ReadLine());
+
+            citas[contador] = new Cita(numero, estudiante, enfermedad, precio);
+            contador++;
+            Console.WriteLine("Cita creada exitosamente.\n");
+        }
+
+        public static void ListarCitas(Cita[] citas, int contador)
+        {
+            double total = 0;
+            Console.WriteLine("Lista de citas:");
+            for (int i = 0; i < contador; i++)
+            {
+                Console.WriteLine(citas[i].ToString());
+                total += citas[i].Precio;
+            }
+            Console.WriteLine($"\nSuma total de precios: {total:C}\n");
+        }
+
+        public static void ModificarUniversidad(Cita[] citas, int contador)
+        {
+            Console.Write("Ingrese texto a buscar en las universidades: ");
+            string buscar = Console.ReadLine();
+            Console.Write("Ingrese texto por el cual reemplazar: ");
+            string reemplazar = Console.ReadLine();
+
+            for (int i = 0; i < contador; i++)
+            {
+                if (citas[i].Estudiante.Universidad.Contains(buscar))
+                {
+                    citas[i].Estudiante.Universidad = citas[i].Estudiante.Universidad.Replace(buscar, reemplazar);
+                }
+            }
+            Console.WriteLine("Modificación realizada.\n");
+        }
+    }
+}
---
+++ ejercicio t3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_t3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cita[] citas = new Cita[100];
+            int contador = 0;
+            int opcion;
+
+            do
+            {
+                Console.WriteLine("Menú de Opciones:");
+                Console.WriteLine("1. Crear Cita");
+                Console.WriteLine("2. Listar Citas");
+                Console.WriteLine("3. Modificar Universidad de Estudiantes");
+                Console.WriteLine("4. Fin");
+                Console.Write("Seleccione una opción: ");
+                opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        CitaUtil.CrearCita(citas, ref contador);
+                        break;
+                    case 2:
+                        CitaUtil.ListarCitas(citas, contador);
+                        break;
+                    case 3:
+                        CitaUtil.ModificarUniversidad(citas, contador);
+                        break;
+                    case 4:
+                        Console.WriteLine("Programa finalizado.");
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida, intente de nuevo.");
+                        break;
+                }
+            } while (opcion != 4);
+        }
+    }
+}
